ba4fcea17584e8d8591fcbbe7e79a166cbadbf33
unknown <mf7753@GDD-28.main.ad.rit.edu>
2015-10-04 19:46:11 -0400

    Made an empty piece class and empty Add method.

--- rotate4/Assets/scripts/piece.cs
+++ rotate4/Assets/scripts/piece.cs
@@ -1,22 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class boardManager : MonoBehaviour {
-
-
-    int[,] board;
+public class piece : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		board = new int[8, 8]
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-	}
-
-	void AddPiece(){
-
 	}
 }
