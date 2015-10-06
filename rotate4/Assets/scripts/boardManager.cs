b3d90514e1ad1dda7b39eb48db93dadfc85b78b7
unknown <pmm9335@GDD-26.main.ad.rit.edu>
2015-10-06 15:04:39 -0400

    6x6 board now, filled with board pieces now, game logic still needs work

--- rotate4/Assets/scripts/boardManager.cs
+++ rotate4/Assets/scripts/boardManager.cs
@@ -3,12 +3,32 @@ using System.Collections;
 
 public class boardManager : MonoBehaviour {
 
-
+    /// <summary>
+    /// 0 = empty, 1 = white, 2 = black
+    /// </summary>
     int[,] board;
+    Vector3 origin = new Vector3(-4.75f, 0.25f, 3.15f);
+    Vector3 spacing = new Vector3(1.9f, 0f, -1.26f);
+
+    public GameObject boardPiece;
+    
 
 	// Use this for initialization
 	void Start () {
-		board = new int[8, 8]
+        board = new int[6, 6];
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                GameObject temp = Instantiate(boardPiece);
+                temp.transform.position = new Vector3(
+                    origin.x + spacing.x*i,
+                    origin.y + spacing.y*0,
+                    origin.z + spacing.z*j);
+            }
+        }
+
+
 	}
 	
 	// Update is called once per frame
