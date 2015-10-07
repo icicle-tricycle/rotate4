0d5b5e979c03407b4b1224e3349ac902f6b5c2f1
unknown <wsw3877@GDD-20.main.ad.rit.edu>
2015-10-06 20:29:06 -0400

    Testing committing Wes's changes - Peter

---
+++ Assets/scripts/boardManager.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class boardManager : MonoBehaviour {
+
+    /// <summary>
+    /// 0 = empty, 1 = white, 2 = black
+    /// </summary>
+    int[,] board;
+    Vector3 origin = new Vector3(-4.75f, 0.25f, 3.15f);
+    Vector3 spacing = new Vector3(1.9f, 0f, -1.26f);
+    Vector3 boardOrigin;
+    Vector3 screenSpacing;
+    public Camera camera;
+
+    public GameObject boardPiece;
+    
+
+	// Use this for initialization
+	void Start () {
+        board = new int[6, 6];
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                GameObject temp = Instantiate(boardPiece);
+                temp.transform.position = new Vector3(
+                    origin.x + spacing.x*i,
+                    origin.y + spacing.y*0,
+                    origin.z + spacing.z*j);
+            }
+        }
+
+        //camera = GetComponent<Camera>();
+        boardOrigin = camera.WorldToScreenPoint(origin);
+        screenSpacing = camera.WorldToScreenPoint(spacing);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButtonDown("Fire1"))// && IsOnBoard())
+        {
+            RaycastHit hit;
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log("Raycasting");
+                if (hit.transform.gameObject.tag == "RowTrigger")
+                {
+                    Debug.Log("I have triggered "+hit.transform.name);
+                }
+            }
+            Debug.Log("Mouse 1 "+Input.mousePosition);
+        }
+	}
+
+	void AddPiece()
+    {
+        
+	}
+
+    void IsOnBoard()
+    {
+        //if()
+    }
+}
