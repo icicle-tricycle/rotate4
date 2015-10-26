23e066b9f9cf372d48d35fee21a55f8ececa9d28
unknown <dwe5866@GDD-28.main.ad.rit.edu>
2015-10-25 20:18:09 -0400

    Added font, splash screen now has a background of the board, the board spins, and there are some pieces on it

---
+++ rotate4/Assets/scripts/startScreenSpin.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class startScreenSpin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.forward * Time.deltaTime * 10);
+		//transform.Rotate(Vector3.up * Time.deltaTime, Space.World);
+	}
+}
